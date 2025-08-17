2b7eb354a7e02bed8ba4f03b86ea1f1feaee0631
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-08-17 11:48:37 +0530

    added jwt authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,13 @@
 import pool from '../db.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import validator from 'validator'
+
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
 
 const getUsers = async (req,res)=>{
     const [rows]= await pool.query("SELECT * FROM users");
@@ -11,9 +20,29 @@ const getById = async(req,res)=>{
 }
 
 const createUser = async(req,res)=>{
-    const {name,email,age} = req.body;
-    const [result] = await pool.query("INSERT INTO users(name,email,age) VALUES(?,?,?)",[name,email,age]);
-    res.json({id:result.insertId, name,email,age});
+    const {name,email,password} = req.body;
+    try {
+       const [rows]= await pool.query("SELECT * FROM users WHERE email= ?",[email]);
+       if(rows.length >0){
+        return res.json({success:false, message:"User already exists with this email"})
+       }
+       if(!validator.isEmail(email)){
+         return res.json({success:false, message:"Enter valid email"});
+       }
+       if(password.length< 8){
+        return res.json({success:false,message:"Enter a strong password"});
+       }
+       // if everything is valid, then hash the password;
+       const salt = await bcrypt.genSalt(10);
+       const hashedPassword = await bcrypt.hash(password,salt);
+       const [result] = await pool.query("INSERT INTO users(name,email,password) VALUES(?,?,?)",[name,email,hashedPassword]);
+       const token = createToken(result.insertId);
+
+       res.json({success:true, id:result.insertId, token});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, error:error.message})
+    }
 };
 
 const updateUser = async(req,res)=>{
